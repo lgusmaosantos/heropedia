5fefcf4bcd8303bbeb59bcdf5ce2cf441022cf1c
Luiz Gabriel <lgusmaosantos@gmail.com>
2021-02-02 04:19:31 -0300

    Adiciona view de listagem de heróis

--- heropedia/urls.py
+++ heropedia/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HeroListView.as_view(), name='hero-listing')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
