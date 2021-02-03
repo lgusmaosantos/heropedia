b3cd24ed3193c36c68c2e23433f329582e0481d3
lgusmaosantos <lgusmaosantos@gmail.com>
2021-02-03 02:51:46 -0300

    Feature: Apagar herói (#4)
    Adiciona a funcionalidade de apagar um registro de herói.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
 from .models import Hero
@@ -39,3 +40,25 @@ class HeroUpdateView(SuccessMessageMixin, UpdateView):
 
     def get_success_url(self) -> str:
         return reverse('hero_listing')
+
+
+class HeroDeleteView(DeleteView):
+    """A view que possibilita a deleção de um herói."""
+    model = Hero
+    success_message = 'Registro apagado com sucesso. Esperamos que Thanos não tenha nada a ver com isso.'
+
+    def get_success_url(self) -> str:
+        return reverse('hero_listing')
+
+    def delete(self, request, *args, **kwargs):
+        """Um override do método `delete` da view para
+        a inclusão da mensagem de confirmação da exclusão.
+        É necessário pois uma `message` está normalmente
+        atrelada a um `form_valid`, que não existe em
+        `DeleteView`.
+
+        Mais detalhes na issue do projeto Django, em:
+        https://code.djangoproject.com/ticket/21936
+        """
+        messages.success(self.request, self.success_message)
+        return super(HeroDeleteView, self).delete(request, *args, **kwargs)
