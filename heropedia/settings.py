b544ca5dce438fd54bc2431f696e5797c279ac75
Luiz Gabriel <lgusmaosantos@gmail.com>
2021-02-05 07:21:11 -0300

    Conf. Heroku

--- heropedia/settings.py
+++ heropedia/settings.py
@@ -141,4 +141,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configure Django App for Heroku.
 import django_heroku
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
