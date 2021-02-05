162647a11122a58b9deb68e2d89c5f77661ec33d
Luiz Gabriel <lgusmaosantos@gmail.com>
2021-02-05 05:59:55 -0300

    Ajuste para deploy no Heroku

--- heropedia/settings.py
+++ heropedia/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from .db_settings import DATABASES
+# from .db_settings import DATABASES
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
