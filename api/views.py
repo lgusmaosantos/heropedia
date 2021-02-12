81f344139943838b68c278a818ae9a3d8d367161
Luiz Gabriel <lgusmaosantos@gmail.com>
2021-02-12 02:19:20 -0300

    Configura busca por campo nome do herói (REST API)

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import HeroSerializer
 from core.models import Hero
-from rest_framework.filters import OrderingFilter
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 # Views e viewsets da API REST
 
@@ -12,6 +12,7 @@ class HeroModelViewSet(ModelViewSet):
     """
     serializer_class = HeroSerializer
     queryset = Hero.objects.all()
-    filter_backends = [OrderingFilter]
+    filter_backends = [OrderingFilter, SearchFilter]
     ordering_fields = ['name']
     ordering = ['name']
+    search_fields = ['name']
